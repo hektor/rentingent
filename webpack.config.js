52c8f029e28e4bc82bac022f89bf2fd7632a907b
hektormisplon <hektormisplon@users.noreply.github.com>
2018-12-04 10:39:52 +0100

    Prettified

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const ProgressBarPlugin = require("progress-bar-webpack-plugin");
-const chalk = require("chalk");
-const semver = require("semver");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
+const semver = require('semver');
 
 module.exports = (env, argv) => {
-  const dev = argv.mode === "development";
-  if (!semver.satisfies(process.versions.node, "<=10.17.0")) {
+  const dev = argv.mode === 'development';
+  if (!semver.satisfies(process.versions.node, '<=10.17.0')) {
     console.error(
       `Incorrect Node version ${
         process.versions.node
@@ -18,14 +18,14 @@ module.exports = (env, argv) => {
     process.exit();
   }
   return {
-    entry: { main: "./src/index.js" },
+    entry: { main: './src/index.js' },
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "main.js"
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
     },
     resolve: {
       alias: {
-        handlebars: "handlebars/dist/handlebars.min.js"
+        handlebars: 'handlebars/dist/handlebars.min.js'
       }
     },
     module: {
@@ -34,59 +34,59 @@ module.exports = (env, argv) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
+            loader: 'babel-loader'
           }
         },
         {
           test: /\.handlebars$/,
-          loader: "text-loader"
+          loader: 'text-loader'
         },
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
-            dev ? "style-loader" : MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader"
+            dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
           ]
         },
         {
           test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-          loader: "url-loader",
+          loader: 'url-loader',
           query: {
             limit: 10000,
-            name: "images/[name].[hash:7].[ext]"
+            name: 'images/[name].[hash:7].[ext]'
           }
         },
         {
           test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
             limit: 1000,
-            name: "fonts/[name].[hash:7].[ext]"
+            name: 'fonts/[name].[hash:7].[ext]'
           }
         },
         {
           test: /\.(webm|mp4)$/,
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "videos/[name].[hash:7].[ext]"
+            name: 'videos/[name].[hash:7].[ext]'
           }
         }
       ]
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: "JS WEBPACK TUTORIAL",
-        template: path.resolve(__dirname, "src/html/index.ejs")
+        title: 'JS WEBPACK TUTORIAL',
+        template: path.resolve(__dirname, 'src/html/index.ejs')
       }),
       new MiniCssExtractPlugin({
-        filename: dev ? "[name].css" : "[name].[hash].css",
-        chunkFilename: dev ? "[id].css" : "[id].[hash].css"
+        filename: dev ? '[name].css' : '[name].[hash].css',
+        chunkFilename: dev ? '[id].css' : '[id].[hash].css'
       }),
-      new CleanWebpackPlugin(["dist"], {}),
+      new CleanWebpackPlugin(['dist'], {}),
       new ProgressBarPlugin({
         format: `  build [:bar] ${chalk.green.bold(
-          ":percent"
+          ':percent'
         )} (:elapsed seconds)`,
         clear: false
       })
