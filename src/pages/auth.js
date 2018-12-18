06a3701f2ecd9f0d0e684a83b23fa9b705fd977c
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-18 20:23:18 +0100

    User to db, validation helper
    Store user type to db
    UI form feedback on sign in/sign up

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -6,8 +6,10 @@ import routes from '../routes';
 // Import the template to use
 const authTemplate = require('../templates/auth.handlebars');
 
+// Firebase
 const { getInstance } = require('../firebase/firebase');
 const firebase = getInstance();
+const database = firebase.database();
 
 export default () => {
   //===============| GOOGLE AUTH FUNCTIONALITY |============== //
@@ -22,6 +24,10 @@ export default () => {
   const googleAuthBtn = document.querySelector('.auth__provider__google');
   const emailEl = document.querySelector('.auth__input__email');
   const passwordEl = document.querySelector('.auth__input__password');
+  const passwordConfirmEl = document.querySelector(
+    '.auth__input__password-confirm'
+  );
+  const formHelperEl = document.querySelector('.auth__form__helper');
 
   // SIGN TYPE SWITCHER
   signTypeBtn.addEventListener('click', e => {
@@ -54,6 +60,8 @@ export default () => {
       // catch errors from auth promise
       .catch(error => {
         console.log(error.message);
+        formHelperEl.setAttribute('class', 'auth__form__helper_active');
+        formHelperEl.textContent = error.message;
       });
   });
 
@@ -62,24 +70,35 @@ export default () => {
     e.preventDefault();
     const email = emailEl.value;
     const password = passwordEl.value;
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(response => {
-        console.log(response);
-        console.log(`Welcome, ${email}!`);
-      })
-      // catch errors from auth promise
-      .catch(error => {
-        console.log(error.message);
-      });
+    const passwordConfirm = passwordConfirmEl.value;
+    if (password === passwordConfirm) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(response => {
+          const user = response.user;
+          addUserToDatabase(user);
+        })
+        // catch errors from auth promise
+        .catch(error => {
+          formHelperEl.setAttribute('class', 'auth__form__helper_active');
+          formHelperEl.textContent = error.message;
+          console.log({
+            'Error message': error.message,
+            'Error Code': error.code
+          });
+        });
+    } else {
+      formHelperEl.setAttribute('class', 'auth__form__helper_active');
+      formHelperEl.textContent = 'Please make sure the passwords match.';
+    }
   });
 
   // Sign In w/ Provider - Google
   googleAuthBtn.addEventListener('click', e => {
     e.preventDefault();
     console.log('Google authentication...');
-    let provider = new firebase.auth().GoogleAuthProvider();
+    let provider = firebase.auth().GoogleAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
@@ -91,5 +110,13 @@ export default () => {
       });
   });
 
+  function addUserToDatabase(user) {
+    const userTypeEl = document.getElementById('switch__user-type');
+    const userType = userTypeEl.checked ? 'kotbaas' : 'student';
+
+    database.ref('user/' + user.uid).set({
+      user_type: userType
+    });
+  }
   // Sign In w/ Provider - Facebook
 };
