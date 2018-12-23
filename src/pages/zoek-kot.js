e56a74bc31d7d0ce27e421f5899c89c1d559f4a8
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-23 12:58:33 +0100

    Koten filteren

--- src/pages/zoek-kot.js
+++ src/pages/zoek-kot.js
@@ -7,24 +7,55 @@ import firebase from 'firebase'; // @@@TODO --> make global functions for fireba
 const zoekKotTemplate = require('../templates/zoek-kot.handlebars');
 
 export default () => {
-  // Data to be passed to the template
-  // const name = 'RentInGent';
-  // // Return the compiled template to the router
-  // update(compile(zoekKotTemplate)({ name }));
+  function dynamicSort(filterBy) {
+    const sortOrder = 1;
+    return function(a, b) {
+      let result =
+        a[filterBy] < b[filterBy] ? -1 : a[filterBy] > b[filterBy] ? 1 : 0;
+      console.log(result * sortOrder);
+      return result * sortOrder;
+    };
+  }
 
-  // add firebase data to client side (indexed db)
-  // check if data is yet to be added
+  const filterKoten = koten => {
+    const filterByEl = document.querySelector('.zoek-kot__filter__dropdown');
+    filterByEl.addEventListener('change', e => {
+      const filterByValue = filterByEl.options[filterByEl.selectedIndex].text;
+      switch (filterByValue) {
+        case 'Price':
+          console.log('price');
+          dynamicSort(filterByValue);
+          console.log()
+          console.log(dynamicSort(filterByValue));
+          break;
+        case 'Type':
+          console.log('type');
+          break;
+        case 'Surface':
+          console.log('surface');
+          break;
+      }
+    });
+
+    // - Type
+    // - Huurprijs (van x - x)
+    // - Oppervlakte
+    // - Afstand (van x - x) @@@ pass in distance
+  };
+
+  // const name = 'RentInGent';
   const ref = firebase.database().ref('/kot');
   ref.once('value', snapshot => {
+    // Data to be passed to the template
     let koten = [];
     snapshot.forEach(kot => {
       const kotKey = kot.key;
       const kotData = kot.val();
       koten.push(new Kot(kotData));
-      console.log(kotData);
     });
-    console.log(koten);
+    // Return the compiled template to the router
     update(compile(zoekKotTemplate)({ koten }));
+    filterKoten(koten);
   });
 };
 
