d01fcc25a8a34cc55b813b2178a76133b6dfaec8
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-26 18:51:21 +0100

    404 - weather in gent

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
   mapBoxToken:
-    'pk.eyJ1IjoiaGVrdHIiLCJhIjoiY2pwOWtucXM0MDA3OTNrbXJjbTRjbXkxNiJ9.YRTsyu3A5BMwtvyqcSWlmA'
+    'pk.eyJ1IjoiaGVrdHIiLCJhIjoiY2pwOWtucXM0MDA3OTNrbXJjbTRjbXkxNiJ9.YRTsyu3A5BMwtvyqcSWlmA',
+  openWeatherMapKey: 'c60aca6c405a0eb94a5d41504ecc8737',
+  openWeatherMapGentId: '2797657'
 };
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,28 @@
 // Only import the compile function from handlebars instead of the entire library
 import { compile } from 'handlebars';
 import update from '../helpers/update';
+import config from '../config';
 
 // Import the template to use
 const pageNotFoundTemplate = require('../templates/404.handlebars');
 
 export default () => {
   // Data to be passed to the template
-  const name = 'Rent@Gent';
-  // Return the compiled template to the router
-  update(compile(pageNotFoundTemplate)({ name }));
+  fetch(
+    'http://api.openweathermap.org/data/2.5/forecast?id=2797657&APPID=c60aca6c405a0eb94a5d41504ecc8737'
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const weather = data.list[5].weather[0].main;
+      console.log(weather);
+      // Return the compiled template to the router
+      update(compile(pageNotFoundTemplate)({ weather }));
+    })
+    .catch(error => {
+      console.log(error);
+      // Return the compiled template to the router
+      update(compile(pageNotFoundTemplate));
+    });
 };
