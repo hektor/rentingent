79a92fe8f6439a94163f6d22a4a7ca77636ab389
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-27 19:19:57 +0100

    init | page mijn-koten; remove bloat

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@
 import HomeView from './pages/home';
 import AuthView from './pages/auth';
 import ZoekKotView from './pages/zoek-kot';
-import AboutView from './pages/about';
 import MapboxView from './pages/mapbox-example';
 import MessagesView from './pages/messages';
+import MijnKotenView from './pages/mijn-koten';
 import KotToevoegenView from './pages/kot-toevoegen';
 import SwipeKotView from './pages/swipeview';
 import pageNotFoundView from './pages/404';
@@ -13,9 +13,9 @@ export default [
   { path: '/home', view: HomeView },
   { path: '/auth', view: AuthView },
   { path: '/zoek-kot', view: ZoekKotView },
-  { path: '/about', view: AboutView },
   { path: '/mapbox', view: MapboxView },
   { path: '/messages', view: MessagesView },
+  { path: '/mijn-koten', view: MijnKotenView },
   { path: '/kot-toevoegen', view: KotToevoegenView },
   { path: '/swipeview', view: SwipeKotView },
   { path: '/404', view: pageNotFoundView }
