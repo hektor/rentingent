c7c026e15ec042d5ae53c6cc0acc83777f6ec790
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-27 19:16:54 +0100

    Dynamic home page - according to usertype

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,23 +4,34 @@ const firebase = getInstance();
 const database = firebase.database();
 
 export default class User {
-  constructor() {
-    this.user = JSON.parse(localStorage.getItem('currentUser'));
-    this.uid = this.user ? this.user.uid : null;
-    this.userType = this.getUserType();
-    // this.userEmail = user.email;
-    // this.userEmailVerified = user.emailVerified;
-  }
+  constructor() {}
 
-  getUser() {
-    return this.user;
-  }
-
-  getUserType() {
-    if (this.uid) {
-      database.ref(`/user/${this.uid}`).on('value', snapshot => {
-        localStorage.setItem('currentUserType', snapshot.val().user_type);
+  getCurrentUser() {
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          resolve(user);
+        } else {
+          reject(console.error('could not get current user'));
+        }
       });
-    }
+    });
+  }
+
+  getCurrentUserType() {
+    return this.getCurrentUser().then(user => {
+      return new Promise((resolve, reject) => {
+        database
+          .ref(`/user/${user.uid}`)
+          .once('value')
+          .then(snapshot => {
+            const userType = snapshot.val().user_type;
+            resolve(userType);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
+    });
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 // Only import the compile function from handlebars instead of the entire library
 import { compile } from 'handlebars';
 import update from '../helpers/update';
+import User from './User';
 
 const { getInstance } = require('../firebase/firebase');
 const firebase = getInstance();
@@ -9,23 +10,28 @@ const firebase = getInstance();
 const homeTemplate = require('../templates/home.hbs');
 
 export default () => {
-  // Data to be passed to the template
-
-  const user = 'user';
-  // Return the compiled template to the router
-
-  // check user-type here and load home page accordingly
-
-  // check if user is activated - if not, load activation-home.handlebars
-
-  update(compile(homeTemplate)({ user }));
-  const logOutBtn = document.querySelector('.btn__sign-out');
-  logOutBtn.addEventListener('click', e => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        localStorage.clear();
+  const currentUser = new User();
+  currentUser
+    .getCurrentUserType()
+    .then(userType => {
+      // check user-type here and load home page accordingly
+      if (userType === 'student') {
+        update(compile(homeTemplate)({}));
+      } else if (userType === 'kotbaas') {
+        let kotbaas = true;
+        update(compile(homeTemplate)({ kotbaas }));
+      }
+    })
+    .then(() => {
+      const logOutBtn = document.querySelector('.btn__sign-out');
+      logOutBtn.addEventListener('click', e => {
+        firebase
+          .auth()
+          .signOut()
+          .then(() => {
+            localStorage.clear();
+          });
       });
-  });
+    });
+  // TODO: check if user is activated and load home page accordingly
 };
