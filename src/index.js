89c56c177a7e4cb5cb6ec3323809fe3a8198f1f0
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-27 19:08:09 +0100

    template default (no auth check)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import Navigo from 'navigo';
 import handlebars, { compile } from 'handlebars';
-import update from './helpers/update';
 
 import './styles/main.sass';
 import routes from './routes';
-import { Koten } from './pages/Kot';
 import User from './pages/User';
 
 // Partials
@@ -16,10 +14,6 @@ const footer = require('./partials/footer.hbs');
 const { getInstance } = require('./firebase/firebase');
 const firebase = getInstance();
 
-const currentUser = new User();
-const userType = currentUser.getUserType();
-console.log(userType);
-
 // Register partials
 handlebars.registerPartial('header-nav', compile(navHeader)({}));
 handlebars.registerPartial('header-info', compile(infoHeader)({}));
@@ -29,18 +23,7 @@ handlebars.registerPartial(
 );
 
 // Router logic to load the correct template when needed
-const router = new Navigo('http://localhost:8080/', true, '#');
-
-// routes.forEach(route => {
-//   router.on(route.path, () => {
-//     route.view();
-//     router.updatePageLinks();
-//   });
-// });
-
-const links = document.getElementsByTagName('a');
-console.log(links);
-
+const router = new Navigo(window.location.origin, true, '#');
 routes.forEach(route => {
   router.on(route.path, () => {
     route.view();
@@ -48,32 +31,9 @@ routes.forEach(route => {
   });
 });
 
-router.on(
-  '/user/edit',
-  () => {
-    // show user edit page
-  },
-  {
-    before: function(done, params) {
-      // doing some async operation
-      done();
-    },
-    after: function(params) {
-      // after resolving
-    },
-    leave: function(params) {
-      // when you are going out of the that route
-    }
-  }
-);
-
 // This catches all non-existing routes and redirects back to the home
 router.notFound(() => {
   router.navigate('/404');
 });
 
 router.resolve();
-
-window.onload = () => {
-  router.navigate(window.location.hash.split('/')[1]);
-};
