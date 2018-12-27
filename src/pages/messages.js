7bbb776ca18bedccbdede44e985bf1f65c7594e7
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-28 00:12:07 +0100

    firebase cloud messaging (not sure if works on localhost)

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -12,7 +12,51 @@ const database = firebase.database();
 
 export default () => {
   update(compile(messagesTemplate)({}));
-  messaging.usePublicVapidKey(
-    'BP2F8O4RFkxajh5cmYpvZ73VmwB-ZE1Wx8OCY9yXmAQhXL9Xo-RqmWF57JeGS3TKFd-ok63x9wJGN6SdxEhYG3o'
-  );
 };
+
+const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey(
+  'BP2F8O4RFkxajh5cmYpvZ73VmwB-ZE1Wx8OCY9yXmAQhXL9Xo-RqmWF57JeGS3TKFd-ok63x9wJGN6SdxEhYG3o'
+);
+
+messaging
+  .requestPermission()
+  .then(() => {
+    console.log('permission granted');
+    return messaging.getToken();
+  })
+  .then(token => {
+    console.log(token);
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
+
+messaging.onMessage(payload => {
+  const message = document.querySelector('.message');
+  console.log(payload);
+  let data = {
+    message: payload.notification.title,
+    timeout: 5000,
+    actionHandler() {
+      location.reload();
+    },
+    actionText: 'Reload'
+  };
+  console.log(data);
+  message.textContent = data;
+});
+
+// Content-Type: application/json
+// Authorization: Bearer ya29.ElqKBGN2Ri_Uz...HnS_uNreA
+
+// {
+//   "message":{
+//     "token" : "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...",
+//     "notification" : {
+//       "body" : "This is an FCM notification message!",
+//       "title" : "FCM Message",
+//       }
+//    }
+// }
