cbb25f5dcac99fcfa4e1f4aba7f18247f00f4d2a
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-28 17:34:23 +0100

    refractoring, add uid when adding kot

--- src/pages/kot-toevoegen.js
+++ src/pages/kot-toevoegen.js
@@ -6,37 +6,50 @@ import update from '../helpers/update';
 const kotToevoegenTemplate = require('../templates/kot-toevoegen.hbs');
 
 // Import Kot constructor function
-import Kot from './Kot';
+import { Kot } from './Kot';
+import User from './User';
 
 export default () => {
   // Data to be passed to the template
   const name = 'Rent@Gent';
-  //test
 
-  // Return the compiled template to the router
-  update(compile(kotToevoegenTemplate)({ name }));
-
-  const kotAddBtn = document.querySelector('.kot__add__submit');
-  kotAddBtn.addEventListener('click', e => {
-    const kot = {
-      Naam: document.querySelector('.kot__name__input').value,
-      Straat: document.querySelector('.kot__street__input').value,
-      Huisnummer: document.querySelector('.kot__street-number__input').value,
-      Plaats: document.querySelector('.kot__city__input').value,
-      Status: document.querySelector('.kot__status__input').value,
-      Waarborg: document.querySelector('.kot__warranty__input').value,
-      Verdieping: document.querySelector('.kot__floor__input').value,
-      'Privé toilet': document.querySelector('.kot__has-toilet__input').value,
-      'Privé keuken': document.querySelector('.kot__has-kitchen__input').value,
-      'Privé douche': document.querySelector('.kot__has-shower__input').value,
-      Opties: document.querySelector('.kot__has-furniture__input').value,
-      'Totale prijs': document.querySelector('.kot__total-price__input').value,
-      Type: document.querySelector('.kot__type__input').value,
-      Oppervlakte: document.querySelector('.kot__surface__input').value
-    };
-    const kotAddMessage = document.querySelector('.kot__add__message');
-    kotAddMessage.textContent = checkFields(kot);
+  // check if user is kotbaas
+  const currentUser = new User();
+  currentUser.getCurrentUserType().then(userType => {
+    if (userType === 'student') {
+      console.log('add router here');
+    } else if (userType === 'kotbaas') {
+      loadTemplate();
+    }
   });
+
+  function loadTemplate() {
+    // Return the compiled template to the router
+    update(compile(kotToevoegenTemplate)({ name }));
+    const kotAddBtn = document.querySelector('.kot__add__submit');
+    kotAddBtn.addEventListener('click', e => {
+      const kot = {
+        Naam: document.querySelector('.kot__name__input').value,
+        Straat: document.querySelector('.kot__street__input').value,
+        Huisnummer: document.querySelector('.kot__street-number__input').value,
+        Plaats: document.querySelector('.kot__city__input').value,
+        Status: document.querySelector('.kot__status__input').value,
+        Waarborg: document.querySelector('.kot__warranty__input').value,
+        Verdieping: document.querySelector('.kot__floor__input').value,
+        'Privé toilet': document.querySelector('.kot__has-toilet__input').value,
+        'Privé keuken': document.querySelector('.kot__has-kitchen__input')
+          .value,
+        'Privé douche': document.querySelector('.kot__has-shower__input').value,
+        Opties: document.querySelector('.kot__has-furniture__input').value,
+        'Totale prijs': document.querySelector('.kot__total-price__input')
+          .value,
+        Type: document.querySelector('.kot__type__input').value,
+        Oppervlakte: document.querySelector('.kot__surface__input').value
+      };
+      const kotAddMessage = document.querySelector('.kot__add__message');
+      kotAddMessage.textContent = checkFields(kot);
+    });
+  }
 
   function checkFields(kot) {
     let message;
@@ -45,7 +58,9 @@ export default () => {
       if (value === '') {
         message = 'Vul alle velden in';
       } else {
-        new Kot(kot).addToDatabase(kot);
+        currentUser.getCurrentUser().then(user => {
+          new Kot(kot).addToDatabase(kot, user.uid);
+        });
         message = 'Kot toegevoegd!';
       }
       return message;
