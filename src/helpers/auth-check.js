56dff289ef09a497cfe95e1e826227f8cb3b6d31
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-29 18:50:18 +0100

    new helper - add user type

--- src/helpers/auth-check.js
+++ src/helpers/auth-check.js
@@ -3,6 +3,7 @@ import routes from '../routes';
 
 const { getInstance } = require('../firebase/firebase');
 const firebase = getInstance();
+const database = firebase.database();
 
 const authCheck = () => {
   return new Promise((resolve, reject) => {
@@ -17,8 +18,21 @@ const authCheck = () => {
   });
 };
 
-const getUser = () => {
-  Promise.all([authCheck(), userType()]);
+const getUserType = () => {
+  return authCheck().then(user => {
+    return new Promise((resolve, reject) => {
+      database
+        .ref(`/user/${user.uid}`)
+        .once('value')
+        .then(snapshot => {
+          const userType = snapshot.val().user_type;
+          resolve(userType);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  });
 };
 
 // Returns true if a user is signed-in.
@@ -27,4 +41,4 @@ const getUser = () => {
 // }
 
 export default authCheck;
-export { getUser };
+export { authCheck, getUserType };
