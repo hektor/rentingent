c282b7c634d08ca08a1b5dda39e79d54aea65de6
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-29 16:28:34 +0100

    add auth check helper

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 // Only import the compile function from handlebars instead of the entire library
 import { compile } from 'handlebars';
 import update from '../helpers/update';
+import authCheck from '../helpers/auth-check';
 import User from './User';
 
 const { getInstance } = require('../firebase/firebase');
@@ -10,28 +11,29 @@ const firebase = getInstance();
 const homeTemplate = require('../templates/home.hbs');
 
 export default () => {
-  const currentUser = new User();
-  currentUser
-    .getCurrentUserType()
-    .then(userType => {
-      // check user-type here and load home page accordingly
-      if (userType === 'student') {
-        update(compile(homeTemplate)({}));
-      } else if (userType === 'kotbaas') {
-        let kotbaas = true;
-        update(compile(homeTemplate)({ kotbaas }));
-      }
-    })
-    .then(() => {
-      const logOutBtn = document.querySelector('.btn__sign-out');
-      logOutBtn.addEventListener('click', e => {
-        firebase
-          .auth()
-          .signOut()
-          .then(() => {
-            localStorage.clear();
-          });
+  authCheck().then(user => {
+    user = new User();
+    user
+      .getCurrentUserType()
+      .then(userType => {
+        // check user-type here and load home page accordingly
+        if (userType === 'student') {
+          update(compile(homeTemplate)({}));
+        } else if (userType === 'kotbaas') {
+          let kotbaas = true;
+          update(compile(homeTemplate)({ kotbaas }));
+        }
+      })
+      .then(() => {
+        const logOutBtn = document.querySelector('.btn__sign-out');
+        logOutBtn.addEventListener('click', e => {
+          firebase
+            .auth()
+            .signOut()
+            .then(() => {
+              localStorage.clear();
+            });
+        });
       });
-    });
-  // TODO: check if user is activated and load home page accordingly
+  });
 };
