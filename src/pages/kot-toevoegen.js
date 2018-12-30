124ac7853d2dc0babdc7b3f921a4ccc849c2beb3
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-30 21:00:10 +0100

    refractoring, add image, clean up

--- src/pages/kot-toevoegen.js
+++ src/pages/kot-toevoegen.js
@@ -6,62 +6,67 @@ import { authCheck, getUserType } from '../helpers/auth-check';
 // Import the template to use
 const kotToevoegenTemplate = require('../templates/kot-toevoegen.hbs');
 
+// Firebase
+const { getInstance } = require('../firebase/firebase');
+const firebase = getInstance();
+
 // Import Kot constructor function
 import { Kot } from './Kot';
-import User from './User';
 
 export default () => {
   Promise.all([authCheck(), getUserType()]).then(userResults => {
     const user = userResults[0];
     const userType = userResults[1];
     if (user && userType === 'kotbaas') {
-      loadTemplate();
+      loadTemplate(user);
     }
   });
-
-  function loadTemplate() {
-    // Return the compiled template to the router
+  // load DOM - event listeners
+  function loadTemplate(user) {
     update(compile(kotToevoegenTemplate)({ name }));
-    const kotAddBtn = document.querySelector('.kot__add__submit');
-    kotAddBtn.addEventListener('click', e => {
-      const kot = {
-        Naam: document.querySelector('.kot__name__input').value,
-        Straat: document.querySelector('.kot__street__input').value,
-        Huisnummer: document.querySelector('.kot__street-number__input').value,
-        Plaats: document.querySelector('.kot__city__input').value,
-        Status: document.querySelector('.kot__status__input').value,
-        Waarborg: document.querySelector('.kot__warranty__input').value,
-        Verdieping: document.querySelector('.kot__floor__input').value,
-        'Privé toilet': document.querySelector('.kot__has-toilet__input').value,
-        'Privé keuken': document.querySelector('.kot__has-kitchen__input')
-          .value,
-        'Privé douche': document.querySelector('.kot__has-shower__input').value,
-        Opties: document.querySelector('.kot__has-furniture__input').value,
-        'Totale prijs': document.querySelector('.kot__total-price__input')
-          .value,
-        Type: document.querySelector('.kot__type__input').value,
-        Oppervlakte: document.querySelector('.kot__surface__input').value
-      };
-      const kotAddMessage = document.querySelector('.kot__add__message');
-      kotAddMessage.textContent = checkFields(kot);
+
+    // let image = document.querySelector('.kot__input__image');
+    // image.addEventListener('change', e => {
+    // });
+
+    document.querySelector('.kot__add').addEventListener('submit', e => {
+      e.preventDefault();
+      submitForm(user);
     });
   }
-
-  function checkFields(kot) {
-    let message;
-    for (let field in kot) {
-      const value = kot[field];
-      if (value === '') {
-        message = 'Vul alle velden in';
-      } else if (isNumber(value)) {
-        return value.toString();
-      } else {
-        currentUser.getCurrentUser().then(user => {
-          new Kot(kot).addToDatabase(kot, user.uid);
-        });
-        message = 'Kot toegevoegd!';
-      }
-      return message;
-    }
-  }
 };
+
+// return input values - trim values
+function getInput(key) {
+  return document
+    .querySelector(`.kot__input__${key}`)
+    .value.toString()
+    .trim();
+}
+// get input (+user uid) and store in db
+function submitForm(user) {
+  const image = document.querySelector('.kot__input__image').files[0];
+  const fileName = 'test';
+
+  const kot = {
+    Naam: getInput('name'),
+    Straat: getInput('street'),
+    Huisnummer: getInput('street-number'),
+    Plaats: getInput('city'),
+    Status: getInput('status'),
+    Waarborg: getInput('warranty'),
+    Verdieping: getInput('floor'),
+    'Privé toilet': getInput('toilet'),
+    'Privé keuken': getInput('kitchen'),
+    'Privé douche': getInput('shower'),
+    Opties: getInput('furniture'),
+    'Totale prijs': getInput('total-price'),
+    Type: getInput('type'),
+    Oppervlakte: getInput('surface'),
+    kotbaas: user.uid
+  };
+
+  new Kot(kot).addToDatabase(kot, image);
+  const messageEl = document.querySelector('.kot__add__message');
+  messageEl.textContent = 'Kot toegevoegd!';
+}
