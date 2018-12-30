4f8f139a1fa1bdf48a71bcde85772ad7445ecc67
Hektor Misplon <hektor.misplon@gmail.com>
2018-12-30 21:02:42 +0100

    send message functionality

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -17,29 +17,32 @@ export default () => {
     const userType = userResults[1];
     if (user) {
       let kotbaas = userType === 'kotbaas' ? true : false;
-      // get messages from db
-      const createdOn = new Date().getTime();
-      console.log(createdOn);
-      database
-        .ref(`/conversation/`)
-        .once('value')
-        .then(snapshot => {
-          snapshot.forEach(chat => {
-            console.log(chat.val());
-          });
-        });
       update(compile(messagesTemplate)({ kotbaas }));
+      const newMessageBtn = document.querySelector('.btn__new-message__toggle');
+      const newMessageEl = document.querySelector('.message__form');
+
+      newMessageBtn.addEventListener('click', e => {
+        newMessageEl.classList.toggle('hidden');
+      });
+      newMessageEl.addEventListener('submit', e => {
+        e.preventDefault();
+        const message = document.querySelector('.new-message__input').value;
+        if (message !== '') {
+          sendMessage(message, user);
+        }
+      });
       requestNotificationPermission();
-      spawnNotification('New message', 'hello world');
+      getMessages();
+      //spawnNotification('New message', 'hello world');
     }
   });
 
   const requestNotificationPermission = () => {
     if (window.Notification && Notification.permission !== 'granted') {
       Notification.requestPermission(status => {
-        if (Notification.permission !== status) {
-          Notification.permission = status;
-        }
+        status === 'granted'
+          ? spawnNotification('Thank you,', 'Notifications are now enabled')
+          : console.log('Notifications blocked');
       });
     }
   };
@@ -52,3 +55,35 @@ export default () => {
     new Notification(title, options);
   };
 };
+
+const sendMessage = (text, user) => {
+  const message = {
+    message: text,
+    sender: user.uid,
+    created_on: new Date().getTime()
+  };
+  return database
+    .ref(`/conversation/${user.uid}`)
+    .push(message)
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+function getMessages() {
+  // Loads the last 12 messages and listen for new ones.
+  const callback = snap => {
+    var data = snap.val();
+    console.log(data);
+    //displayMessage(snap.key, data.message, data.sender, data.created_on);
+  };
+
+  database
+    .ref('/conversation/')
+    .limitToLast(12)
+    .on('child_added', callback);
+  database
+    .ref('/conversation/')
+    .limitToLast(12)
+    .on('child_changed', callback);
+}
