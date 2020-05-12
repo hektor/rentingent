f1a41de07b9e11b84d56f76d4bcfa6b620beecd0
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-12 20:58:20 +0000

    Add layout component

---
+++ src/components/layout.component.svelte
@@ -0,0 +1,8 @@
+<script>
+import Header from './header.component.svelte'
+</script>
+
+<div class="container">
+  <Header />
+  <slot />
+</div>
--- src/pages/home.page.svelte
+++ src/pages/home.page.svelte
@@ -1,9 +1,10 @@
 <script>
+import Layout from '../components/layout.component.svelte'
 import Button from '../components/button.component.svelte'
 </script>
 
-<div>
+<Layout>
   <h1>Home</h1>
   <Button title="Button"/>
-</div>
+</Layout>
 
