18262740aeb823fdad75c5e5ad5db88e95338c56
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-13 20:22:48 +0000

    Add button styling props

--- src/components/button.component.svelte
+++ src/components/button.component.svelte
@@ -2,19 +2,37 @@
   export let title;
   export let type;
   export let name;
+  export let outline = false;
+  export let fg = 'var(--color-5)';
+  export let bg = "var(--color-1)"
 </script>
 
-<button on:click name={name} type={type}>{title}</button>
-
+<button 
+  class:outline 
+  on:click name={name} 
+  style="--fg-color:{fg}; --bg-color:{bg}"
+  type={type}
+>
+  {title}
+</button>
+  
 <style>
   button {
     padding: 1rem;
     border-radius: 0.25rem;
-    border: 1px solid #ccc;
     font-size: 1.25rem;
     margin: 2rem 0;
     text-align: left;
     border: none;
     font-family: inherit;
+    color: var(--fg-color);
+    background: var(--bg-color);
+    cursor: pointer;
+  }
+
+  .outline {
+    border: 2px solid var(--fg-color);
+    background: transparent;
+    color: var(--fg-color);
   }
 </style>
