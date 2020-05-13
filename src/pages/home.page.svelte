ae03b424322c7d278dd1b8cf2703ab6344a2a7fa
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-13 13:02:18 +0000

    Use header on homepage

--- src/pages/home.page.svelte
+++ src/pages/home.page.svelte
@@ -3,7 +3,7 @@ import Layout from '../components/layout.component.svelte'
 import Button from '../components/button.component.svelte'
 </script>
 
-<Layout>
+<Layout withHeader>
   <h1>Home</h1>
   <Button title="Button"/>
 </Layout>
