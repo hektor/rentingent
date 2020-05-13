46661098e36f1fd37861af851fb335e3d269b768
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-13 12:59:55 +0000

    Style button component

--- src/components/button.component.svelte
+++ src/components/button.component.svelte
@@ -4,13 +4,17 @@
   export let name;
 </script>
 
-<button name={name} type={type}>{title}</button>
+<button on:click name={name} type={type}>{title}</button>
 
 <style>
   button {
-    background: none;
+    padding: 1rem;
+    border-radius: 0.25rem;
+    border: 1px solid #ccc;
+    font-size: 1.25rem;
+    margin: 2rem 0;
+    text-align: left;
     border: none;
-    padding: .5rem;
-    margin: .25rem;
+    font-family: inherit;
   }
 </style>
