abafed4beecf8a2aafeb7beab4200b2a1c3afd7c
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-13 17:59:10 +0000

    Add splash screen

--- src/components/app.component.svelte
+++ src/components/app.component.svelte
@@ -2,6 +2,15 @@
   import { Router, Route } from "svelte-routing";
   import Auth from '../pages/auth.page.svelte'
   import Home from '../pages/home.page.svelte'
+
+  const hideSplashScreen = () => {
+    setTimeout(() => {
+      document.querySelector(".app-loading").classList.remove("app-loading")
+    }, 150);
+  }
+
+  hideSplashScreen();
+
 </script>
 
 <Router>
